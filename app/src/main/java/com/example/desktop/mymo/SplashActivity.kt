f1cf2fcb9d3bb2db5d347f2215267822d4a8ce97
김영진 <youngjin033@ajou.ac.kr>
2020-11-01 23:35:33 +0900

    로딩 화면 생성

---
+++ app/src/main/java/com/example/desktop/mymo/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.desktop.mymo
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
