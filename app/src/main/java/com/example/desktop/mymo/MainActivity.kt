51b50908a7993657785818b2a11d4d4e4507d52c
김영진 <youngjin033@ajou.ac.kr>
2020-11-05 23:38:36 +0900

    툴바 디자인 변경 및 기능 추가

--- app/src/main/java/com/example/desktop/mymo/MainActivity.kt
+++ app/src/main/java/com/example/desktop/mymo/MainActivity.kt
@@ -3,18 +3,52 @@ package com.example.desktop.mymo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         val fab: View = findViewById(R.id.fab)
         fab.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java)
             startActivity(intent)
         }
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.baseline_menu_white_24dp)
+        supportActionBar!!.setDisplayShowTitleEnabled(true)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainmenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item!!.itemId){
+            android.R.id.home->{
+                Snackbar.make(toolbar,"Menu pressed",Snackbar.LENGTH_SHORT).show()
+            }
+            R.id.menu_search->{
+                Snackbar.make(toolbar,"Search Menu pressed",Snackbar.LENGTH_SHORT).show()
+            }
+            R.id.menu_account->{
+                Snackbar.make(toolbar,"Account Menu pressed",Snackbar.LENGTH_SHORT).show()
+            }
+            R.id.menu_logout->{
+                Snackbar.make(toolbar,"Logout Menu pressed",Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
