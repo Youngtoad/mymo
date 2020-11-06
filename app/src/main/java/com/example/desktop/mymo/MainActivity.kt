8a9d614b9a2b34c82e0a6e694d694ee3f888e17b
김영진 <youngjin033@ajou.ac.kr>
2020-11-06 22:46:56 +0900

    네비게이션 드로어 생성

--- app/src/main/java/com/example/desktop/mymo/MainActivity.kt
+++ app/src/main/java/com/example/desktop/mymo/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.desktop.mymo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,15 +30,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.mainmenu, menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         when(item!!.itemId){
             android.R.id.home->{
-                Snackbar.make(toolbar,"Menu pressed",Snackbar.LENGTH_SHORT).show()
+                drawerLayout.openDrawer(GravityCompat.START)
             }
             R.id.menu_search->{
                 Snackbar.make(toolbar,"Search Menu pressed",Snackbar.LENGTH_SHORT).show()
@@ -47,8 +49,26 @@ class MainActivity : AppCompatActivity() {
                 Snackbar.make(toolbar,"Logout Menu pressed",Snackbar.LENGTH_SHORT).show()
             }
         }
-
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.list_memo->Snackbar.make(toolbar,"메모 목록",Snackbar.LENGTH_SHORT).show()
+            R.id.star->Snackbar.make(toolbar,"즐겨찾기",Snackbar.LENGTH_SHORT).show()
+            R.id.trash->Snackbar.make(toolbar,"휴지통",Snackbar.LENGTH_SHORT).show()
+            R.id.setting->Snackbar.make(toolbar,"설정",Snackbar.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawers()
+        return false
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers()
+        }else{
+            super.onBackPressed()
+        }
     }
 
 }
