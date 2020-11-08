c8c7f02669f9a5f15018a964e9e687cc760a8804
김영진 <youngjin033@ajou.ac.kr>
2020-11-08 23:29:43 +0900

    메모장 추가 액티비티 수정 및 툴바 설정

--- app/src/main/java/com/example/desktop/mymo/EditActivity.kt
+++ app/src/main/java/com/example/desktop/mymo/EditActivity.kt
@@ -1,16 +1,25 @@
 package com.example.desktop.mymo
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+        setSupportActionBar(toolbar)
 
-        val actionBar = supportActionBar
-        if(actionBar != null){
-            actionBar.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            android.R.id.home-> onBackPressed()
         }
+        return super.onOptionsItemSelected(item)
     }
 }
