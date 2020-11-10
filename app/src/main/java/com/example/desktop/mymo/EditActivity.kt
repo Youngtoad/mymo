7f99d511f33c4c042d4d550b307370981cfda21c
김영진 <youngjin033@ajou.ac.kr>
2020-11-10 23:14:43 +0900

    edit 액티비티 툴바 변경 및 툴바 폰트 설정

--- app/src/main/java/com/example/desktop/mymo/EditActivity.kt
+++ app/src/main/java/com/example/desktop/mymo/EditActivity.kt
@@ -1,9 +1,11 @@
 package com.example.desktop.mymo
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class EditActivity : AppCompatActivity() {
@@ -16,9 +18,15 @@ class EditActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayShowTitleEnabled(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.edit_menu, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item!!.itemId){
             android.R.id.home-> onBackPressed()
+            R.id.menu_store -> Snackbar.make(toolbar,"저장되었습니다!", Snackbar.LENGTH_SHORT).show()
         }
         return super.onOptionsItemSelected(item)
     }
