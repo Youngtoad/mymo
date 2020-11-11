c3bcd53fb77e5f6a223702ae9ed185e6c2ff2d82
김영진 <youngjin033@ajou.ac.kr>
2020-11-11 23:01:47 +0900

    SharedPreference 생성 및 사용자 프로필 저장

--- app/src/main/java/com/example/desktop/mymo/MainActivity.kt
+++ app/src/main/java/com/example/desktop/mymo/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.desktop.mymo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
@@ -17,6 +19,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        MyApplication.prefs.setString("email", "test@ajou.ac.kr")
+        MyApplication.prefs.setString("name", "Youngtoad")
+
+
 
         val fab: View = findViewById(R.id.fab)
         fab.setOnClickListener {
@@ -37,6 +43,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item!!.itemId){
             android.R.id.home->{
+                val tv_nav_em = findViewById<TextView>(R.id.nav_email)
+                val tv_nav_name = findViewById<TextView>(R.id.nav_name)
+
+                tv_nav_em.setText(MyApplication.prefs.getString("email", "no email"))
+                tv_nav_name.text = MyApplication.prefs.getString("name", "no name")
+
                 drawerLayout.openDrawer(GravityCompat.START)
             }
             R.id.menu_search->{
