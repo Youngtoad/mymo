9a13d6ce16147dc1297f6fdc540c2cd51c4f94dd
김영진 <youngjin033@ajou.ac.kr>
2020-12-01 23:26:23 +0900

    데이터 삽입 기능 추가

--- app/src/main/java/com/example/desktop/mymo/EditActivity.kt
+++ app/src/main/java/com/example/desktop/mymo/EditActivity.kt
@@ -6,14 +6,18 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_edit.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.toolbar
 
 class EditActivity : AppCompatActivity() {
+    var db : MemoDatabase? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
         setSupportActionBar(toolbar)
 
+        db = MemoDatabase.getInstance(this)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowTitleEnabled(true)
     }
@@ -27,7 +31,10 @@ class EditActivity : AppCompatActivity() {
         when(item!!.itemId){
             android.R.id.home-> onBackPressed()
             R.id.menu_store -> {
-
+                val title = edit_title.text.toString()
+                val contents = edit_contents.text.toString()
+                val memo = Memo(0, title, contents)
+                db?.MemoDao()?.insertAll(memo)
                 Snackbar.make(toolbar,"저장되었습니다!", Snackbar.LENGTH_SHORT).show()
             }
         }
