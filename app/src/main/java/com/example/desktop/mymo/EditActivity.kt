9d1c0acf572feed68c7ff7b14d1550ef99582cd8
김영진 <youngjin033@ajou.ac.kr>
2020-12-02 23:35:19 +0900

    삽입 기능 수정

--- app/src/main/java/com/example/desktop/mymo/EditActivity.kt
+++ app/src/main/java/com/example/desktop/mymo/EditActivity.kt
@@ -1,5 +1,6 @@
 package com.example.desktop.mymo
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -11,13 +12,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.toolbar
 
 class EditActivity : AppCompatActivity() {
-    var db : MemoDatabase? = null
+    var memoDb : MemoDatabase? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
         setSupportActionBar(toolbar)
 
-        db = MemoDatabase.getInstance(this)
+
+
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowTitleEnabled(true)
     }
@@ -28,13 +31,22 @@ class EditActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        memoDb = MemoDatabase.getInstance(this)
         when(item!!.itemId){
             android.R.id.home-> onBackPressed()
             R.id.menu_store -> {
-                val title = edit_title.text.toString()
-                val contents = edit_contents.text.toString()
-                val memo = Memo(0, title, contents)
-                db?.MemoDao()?.insertAll(memo)
+                val addRunnable = Runnable {
+                    val newMemo = Memo()
+                    newMemo.title = edit_title.text.toString()
+                    newMemo.contents = edit_contents.text.toString()
+                    memoDb?.memoDao()?.insert(newMemo)
+                }
+
+                val addThread = Thread(addRunnable)
+                addThread.start()
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
                 Snackbar.make(toolbar,"저장되었습니다!", Snackbar.LENGTH_SHORT).show()
             }
         }
